6b5db7ee50ad206313ef3ab328d2e675dcf55f01
Valentyn <wolf.valentyn@gmail.com>
2023-08-11 17:20:46 +0300

    implement modal open and close functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import './slider';
+import modals from './modules/modals';
 
-console.log('hello')
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+})
---
+++ src/js/modules/modals.js
@@ -0,0 +1,43 @@
+const modals = () => {
+    const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+        const triggers = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector);
+
+        triggers.forEach(trigger => {
+            trigger.addEventListener('click', e => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            });
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+
+        modal.addEventListener('click', e => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+        });
+    };
+
+    const showModalByTime = (modalSelector, time) => {
+        setTimeout(() => {
+            document.querySelector(modalSelector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time)
+    };
+    
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 60000);
+}
+
+export default modals;
