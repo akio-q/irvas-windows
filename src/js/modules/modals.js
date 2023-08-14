04adfd612fe6278ed09b2882e74ca845d87bf77a
Valentyn <wolf.valentyn@gmail.com>
2023-08-14 14:18:04 +0300

    add some additional functionality to modals
    add closeAllModals method which closes all the modals and use this method when closing or opening any modal, add closeClickOverlay argument which determines whether modal will close when you click on hte overlay or not

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,14 +1,23 @@
 const modals = () => {
-    const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+    const bindModal = (triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) => {
         const triggers = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]');
+
+        const closeAllModals = () => {
+            windows.forEach(item => {
+                item.style.display = 'none'
+            })
+        }
 
         triggers.forEach(trigger => {
             trigger.addEventListener('click', e => {
                 if (e.target) {
                     e.preventDefault();
                 }
+
+                closeAllModals();
     
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
@@ -16,12 +25,16 @@ const modals = () => {
         });
 
         close.addEventListener('click', () => {
+            closeAllModals();
+
             modal.style.display = 'none';
             document.body.style.overflow = '';
         });
 
         modal.addEventListener('click', e => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                closeAllModals();
+
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
             }
@@ -35,7 +48,7 @@ const modals = () => {
         }, time)
     };
     
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close', false);
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     showModalByTime('.popup', 60000);
